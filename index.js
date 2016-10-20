c2ffccab1a6ad4ade9f33eacb647997b8c2ff964
extwiii <bcagiran@hotmail.com>
2016-10-20 18:03:37 +0100

    updating port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = process.env.PORT || 3700; 
+var port = process.env.PORT || 3700;
 
 // Set view of '/' end point
 app.set('views', __dirname + '/views');
@@ -13,10 +13,11 @@ app.get("/", function(req, res){
 // use our puclic/chat.js file as listener
 app.use(express.static(__dirname + '/public'));
 // Set port
-var io = require('socket.io').listen(
-			app.listen(port, function () {
-  				console.log('Node.js listening on port ' + port);
-		}));
+var midPort = app.listen(port, function () {
+    console.log('Node.js listening on port ' + port);
+})
+
+var io = require('socket.io').listen(midPort);
 // set up socket connection
 io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'Welcome to the Real Time Web Chat' });
@@ -24,7 +25,3 @@ io.sockets.on('connection', function (socket) {
         io.sockets.emit('message', data);
     });
 });
-
-
-
-
